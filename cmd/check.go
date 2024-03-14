2be4a197ad1292fd245169072990c70d74fe119c
Swandono <me@swandono.com>
2024-03-15 01:03:11 +0700

    feat: playaround with std library

--- cmd/check.go
+++ cmd/check.go
@@ -5,7 +5,10 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -20,30 +23,58 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("check called")
-	},
+	Run: command,
 }
 
 func init() {
 	rootCmd.AddCommand(checkCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// checkCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// checkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	out, err := exec.Command("which", "tldr").Output()
-	if err != nil {
-		fmt.Println("Error: ", err)
-        out, err = exec.Command("brew", "install", "tldr").Output()
-        if err != nil {
-            fmt.Println("Error: ", err)
-        }
+func command(cmd *cobra.Command, args []string) {
+	// Check if there are any arguments
+	if len(args) == 0 {
+		fmt.Println("No arguments provided")
+		return
 	}
-	fmt.Println("Output: ", string(out))
+	gitURL := fmt.Sprintf("https://github.com/%s", args[0])
+
+	// Create a temp directory
+	dir, err := os.MkdirTemp(".tmp", "sidewalk-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	// Clone the repository
+	_, err = exec.Command("git", "clone", gitURL, dir).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// List the items in the directory
+	output, err := exec.Command("ls", dir).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Output list items: ", string(output))
+
+	// Convert []byte to string then make it list with delimiter \n
+	items := strings.Split(string(output), "\n")
+
+	// Check if list items already installed
+	for _, item := range items {
+		fmt.Println("Checking ", item)
+		_, err := exec.Command("brew", "list", string(item)).Output()
+		if err != nil {
+			fmt.Println("Not installed ", item)
+			// fmt.Println("Installing ", item)
+			// _, err := exec.Command("brew", "install", string(item)).Output()
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+		} else {
+			fmt.Println("Already installed ", item)
+		}
+	}
+
 }
